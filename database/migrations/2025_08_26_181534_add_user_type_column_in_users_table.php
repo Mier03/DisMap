799d67740d3c7d22224a08b4691b1e50b9470e9b
Mier <mierangelina47@gmail.com>
2025-08-27 02:18:17 +0800

    feat: add user_type column in users table

---
+++ database/migrations/2025_08_26_181534_add_user_type_column_in_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+             $table->string('user_type')
+                  ->default('user')
+                  ->comment('Defines the type of user: admin, doctor, nurse, or patient')
+                  ->after('email'); // optional: put it after email
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('user_type');
+        });
+    }
+};
