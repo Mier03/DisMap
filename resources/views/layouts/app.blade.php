2f868e9ed071313b99236221eb0c06b392a5733c
arariadne <elaishaarias@gmail.com>
2025-09-12 10:33:21 +0800

    landing page issue fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- <title>{{ config('app.name', 'Laravel') }}</title> -->
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -27,9 +27,9 @@
                 </header>
             @endisset
 
-            
-            <main> 
-                @yield('content')
+            <!-- Page Content -->
+            <main>
+                {{ $slot }}
             </main>
         </div>
     </body>
