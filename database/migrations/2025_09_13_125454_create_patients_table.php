5793a30153f67cc9af33a5ec49ddc0183e9f2607
Rainelyn Sungahid <rainelynsungahid@gmail.com>
2025-09-14 01:36:58 +0800

    migrations added for patients and barangay
    migrations added for patients and barangay

---
+++ database/migrations/2025_09_13_125454_create_patients_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('age');
+            $table->foreignId('barangay_id')->constrained()->onDelete('cascade');
+            $table->string('diagnosis');
+            $table->string('username')->unique();
+            $table->string('email')->unique();
+            $table->date('date_reported')->nullable();
+            $table->string('status')->default('Active');
+            $table->timestamps();
+
+            $table->unsignedBigInteger('hospital_id')->nullable();
+            $table->foreign('hospital_id')->references('id')->on('hospitals_table')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('patients');
+    }
+};
