a283e429e5ece00653d0c276dd4e3e312a6f1548
Angelina Mier <mierangelina03@gmail.com>
2025-09-27 02:48:32 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,7 +45,7 @@
                 </x-stat-card>
                 <x-stat-card :value="$totalActiveCases" label="Total Active Cases">
                     <x-slot name="icon">
-                        @svg('gmdi-person-search-o', 'h-12 w-12 text-g-dark')
+                        @svg('gmdi-search', 'h-12 w-12 text-g-dark')
                     </x-slot>
                 </x-stat-card>
                 <x-stat-card :value="$totalCriticalCases" label="Total Critical Cases">
@@ -88,7 +88,7 @@
                             placeholder="Search diseases, locations, barangays..."
                             class="w-full rounded-lg px-4 py-2.5 focus:outline-none focus:ring-2 focus:ring-g-dark text-gray-700 border border-g-dark">
                         <div class="absolute inset-y-0 right-0 flex items-center pr-3">
-                            @svg('gmdi-search-o', 'w-5 h-5 text-gray-400')
+                            @svg('gmdi-search', 'w-5 h-5 text-gray-400')
                         </div>
                     </div>
                 </div>
