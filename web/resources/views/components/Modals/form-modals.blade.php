816b91d6ea198a268cdd6234a0b6d44136677db7
Angelina Mier <mierangelina03@gmail.com>
2025-10-28 10:48:47 +0800

    Revert "Merge branch 'main' into DISMAP"
    This reverts commit e58e68293bee1d8393fe980fe910d44d2c5b246c, reversing
    changes made to 681b56b8fef092b194a9a9def09a63fadf9f1767.

--- web/resources/views/components/Modals/form-modals.blade.php
+++ web/resources/views/components/Modals/form-modals.blade.php
@@ -434,14 +434,14 @@
         icon="lock"
     >
         {{-- Flash Message (Success) --}}
-        @if (session('status') === 'password-updated')
+        <!-- @if (session('status') === 'password-updated')
             <div class="p-3 mb-4 text-green-800 bg-green-100 rounded-lg text-sm">
                 Password updated successfully.
             </div>
-        @endif
+        @endif -->
 
         {{-- Form --}}
-        <form method="POST" action="{{ route('password.update.user') }}" class="space-y-5">
+        <form id="passwordForm" method="POST" action="{{ route('password.update.user') }}" class="space-y-5">
             @csrf
             @method('PUT')
              {{-- Current Password --}}
@@ -838,11 +838,13 @@
         document.getElementById('export_disease_id').value = "";
     }
     document.addEventListener('DOMContentLoaded', function() {
-            const modal = document.getElementById('addHospitalModal');
-            if (modal) {
-                modal.classList.remove('hidden');
-                modal.classList.add('flex');
+        @if (isset($errors) && $errors->updatePassword->any())
+            const pwdModal = document.getElementById('passwordUpdateModal');
+            if (pwdModal) {
+                pwdModal.classList.remove('hidden');
+                pwdModal.classList.add('flex');
             }
+        @endif
         });
     const addHospitalModal = document.getElementById('addHospitalModal');
     if (addHospitalModal) {
@@ -1131,3 +1133,4 @@
     });
 </script>
 @endif
+
