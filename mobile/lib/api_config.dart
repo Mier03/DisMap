f4fdbf6ac46cd2cdd101fe06ac89f4a963d85277
Angelina Mier <mierangelina03@gmail.com>
2025-11-30 18:49:07 +0800

    Update api_config.dart

--- mobile/lib/api_config.dart
+++ mobile/lib/api_config.dart
@@ -1,5 +1,6 @@
 class ApiConfig {
   // Change this only when deploying
-  static const String baseUrl = "http://10.0.2.2:8000/api/";
-  //  static const String baseUrl = "https://dismap.yottahive.com/api/";
+   // static const String baseUrl = "http://10.0.2.2:8000/api/"; // for Android emulator, android studio
+  // static const String baseUrl = "http://127.0.0.1:8000/api/"; //for local testing
+   static const String baseUrl = "https://dismap.yottahive.com/api/"; //for production
 }
